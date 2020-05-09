3eb7f355357b0e30402c4ddd846e03ca631f9f32
FrovaHappy <happypro5.0@gmail.com>
2020-05-09 15:31:09 -0300

    calcular 2 y 3 estan funcional

--- SemI_TP2_1/MainWindow.xaml.vb
+++ SemI_TP2_1/MainWindow.xaml.vb
@@ -1,6 +1,7 @@
 ﻿Class MainWindow
     Dim a, b, c, x, y, z, resultado_cache As Double
     Dim comprovacion As String
+
     Private Function Cargar()
         Try 'captura error
             If comprovacion = "primera" Then
@@ -23,6 +24,7 @@
         End Try
         Return True 'devuelve verdadero en caso de que no se produsca error
     End Function
+
     Private Sub Calcular_1_Click(sender As Object, e As RoutedEventArgs) Handles Calcular_1.Click
         comprovacion = "primera"
         If Cargar() = True Then ' si no hay error ejecuta
@@ -30,6 +32,26 @@
             Label_Resultado_1.Content = Convert.ToString(resultado_cache)
         Else
             Label_Resultado_1.Content = "Null"
+        End If
+    End Sub
+
+    Private Sub Calcular_2_Click(sender As Object, e As RoutedEventArgs) Handles Calcular_2.Click
+        comprovacion = "segunda"
+        If Cargar() = True Then ' si no hay error ejecuta
+            resultado_cache = (((16 * x) + (2 * y - 8)) / (4 * Math.Pow(a, 2.0)))
+            Label_Resultado_2.Content = Convert.ToString(resultado_cache)
+        Else
+            Label_Resultado_2.Content = "Null"
+        End If
+    End Sub
+
+    Private Sub Calcular_3_Click(sender As Object, e As RoutedEventArgs) Handles Calcular_3.Click
+        comprovacion = "tercera"
+        If Cargar() = True Then ' si no hay error ejecuta
+            resultado_cache = (((3 * x) + (2 * y)) / (2 * Math.Pow(z, 2.0)))
+            Label_Resultado_3.Content = Convert.ToString(resultado_cache)
+        Else
+            Label_Resultado_3.Content = "Null"
         End If
     End Sub
 
